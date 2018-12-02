fcb7fbc244cf95e7195adb71e6340c1f6eb02953
Theo Penavaire <theo.penavaire@epitech.eu>
2018-12-02 13:40:53 -0500

    Clean utils file

--- utils.cpp
+++ utils.cpp
@@ -1,6 +1,1 @@
 #include "utils.hpp"
-
-bool fileExists(std::string const &path) {
-  struct stat buff;   
-  return (stat (path.c_str(), &buff) == 0); 
-}
--- utils.hpp
+++ utils.hpp
@@ -1,12 +1,8 @@
 #ifndef UTILS_HPP
 # define UTILS_HPP
 
-# include <string>
-# include <sys/stat.h>
 # include <map>
 # include "LanguageModel.hpp"
-
-bool fileExists(std::string const &path);
 
 using LMMap = std::map<std::string, LanguageModel>;
 
